8bd230dc08f48bb6918219b1cefae7a346548a59
christopher nguyen <christoph.nguyen@gmail.com>
2014-02-20 01:31:01 -0800

    We can now receive messages and decode them and then notify the room with a deltaco counter

--- server.go
+++ server.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/lestopher/hipchat-webhooks/room_message"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 // Global so that we can invoke from handler
 var oauthToken string
+var delTacoCounter int
+var room_notification string
 
 func main() {
 	oauthToken = os.Getenv("HC_OAUTH_TOKEN")
@@ -19,6 +24,7 @@ func main() {
 		log.Fatalln("Envionrment variable HC_OAUTH_TOKEN wasn't set")
 	}
 
+	room_notification = "https://api.hipchat.com/v2/room/447199/notification?auth_token=" + oauthToken
 	r := mux.NewRouter()
 	r.HandleFunc("/deltaco", DelTacoHandler).Methods("POST")
 	http.Handle("/", r)
@@ -38,6 +44,46 @@ func DelTacoHandler(rw http.ResponseWriter, r *http.Request) {
 		log.Fatalf("OMG, messed up decoding\n%s", err)
 	}
 
+	delTacoCounter++
+	go NotifyRoom()
+
 	log.Printf("%v\n", res)
 	rw.WriteHeader(http.StatusNoContent)
+}
+
+func NotifyRoom() {
+	type RoomNotification struct {
+		Color         string `json:"color"`
+		Message       string `json:"message"`
+		MessageFormat string `json:"message_format"`
+	}
+
+	n := RoomNotification{
+		Color:         "yellow",
+		Message:       "(deltaco) Del Taco (deltaco) has been mentioned " + strconv.Itoa(delTacoCounter) + " times.",
+		MessageFormat: "text",
+	}
+
+	b, errJson := json.Marshal(n)
+
+	if errJson != nil {
+		log.Println("error marshalling data")
+		return
+	}
+
+	log.Println(string(b))
+
+	res, err := http.Post(room_notification, "application/json", bytes.NewBuffer(b))
+	defer res.Body.Close()
+
+	if err != nil {
+		log.Println("issue posting", err)
+	}
+
+	body, errReadBody := ioutil.ReadAll(res.Body)
+
+	if errReadBody != nil {
+		log.Println("error reading response body", errReadBody)
+	}
+	log.Println("response: ", body)
 }
