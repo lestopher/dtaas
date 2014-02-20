f78652dd25de8189ae80653000e9cc44042a91aa
christopher nguyen <christoph.nguyen@gmail.com>
2014-02-19 21:56:55 -0800

    Split out struts into their own library and added check for an env variable

--- server.go
+++ server.go
@@ -3,58 +3,22 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/lestopher/hipchat-webhooks/room_message"
 	"log"
 	"net/http"
+	"os"
 )
 
-type RoomMessage struct {
-	Event string `json:"event"`
-	Item  Item   `json:"item"`
-}
-
-type Item struct {
-	Message       Message `json:"message"`
-	Room          Room    `json:"room"`
-	OauthClientId string  `json:"oauth_client_id"`
-	WebhookId     string  `json:"webhook_id"`
-}
-
-type Message struct {
-	Date string `json:"date"`
-	File string `json:"file"`
-	From struct {
-		Id    int32 `json:"id"`
-		Links struct {
-			Self string `json:"self"`
-		} `json:"links"`
-		MentionName string `json:"mention_name"`
-		Name        string `json:"name"`
-	} `json:"from"`
-	Id       int32     `json:"id"`
-	Mentions []Mention `json:"mentions"`
-	Message  string    `json:"message"`
-}
-
-type Mention struct {
-	Id    int32 `json:"id"`
-	Links struct {
-		Self string `json:"self"`
-	} `json:"links"`
-	MentionName string `json:"mention_name"`
-	Name        string `json:"name"`
-}
-
-type Room struct {
-	Id    int32 `json:"id"`
-	Links struct {
-		Members  string `json:"members"`
-		Self     string `json:"self"`
-		Webhooks string `json:"webhooks"`
-	} `json:"links"`
-	Name string `json:"name"`
-}
+// Global so that we can invoke from handler
+var oauthToken string
 
 func main() {
+	oauthToken = os.Getenv("HC_OAUTH_TOKEN")
+
+	if len(oauthToken) < 1 {
+		log.Fatalln("Envionrment variable HC_OAUTH_TOKEN wasn't set")
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/deltaco", DelTacoHandler).Methods("POST")
 	http.Handle("/", r)
@@ -66,7 +30,7 @@ func DelTacoHandler(rw http.ResponseWriter, r *http.Request) {
 	log.Println("Yay, I got a request.")
 
 	decoder := json.NewDecoder(r.Body)
-	var res RoomMessage
+	var res room_message.RoomMessage
 
 	err := decoder.Decode(&res)
 
